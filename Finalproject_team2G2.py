10a2b2c7affb81d4713721a3411e250ce0841259
YuzY-DS <ratanak86908656@gmail.com>
2025-03-10 14:37:58 +0700

    new

--- Finalproject_team2G2.py
+++ Finalproject_team2G2.py
@@ -503,7 +503,4 @@ def main():
             input("Press Enter to continue...")
 
 if __name__ == '__main__':
-    main()  ## testing 
-    ## #######################
-    ##iiiiiiiii
-    
+    main()  ## testing
